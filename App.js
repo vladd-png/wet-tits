bf53e7e72dfbb1caa0886929ee96cb2d5e0702dd
Virginia Ladd <virginialadd2@gmail.com>
2020-04-28 18:07:43 -0600

    Add basic custom styling of two buttons

--- App.js
+++ App.js
@@ -6,109 +6,58 @@
  * @flow strict-local
  */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+ import React from 'react';
+ import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+ function Separator() {
+   return <View style={styles.separator} />;
+ }
 
-const App: () => React$Node = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
-};
+ export default function App() {
+   return (
+     <SafeAreaView style={styles.container}>
+       <View>
+         <Button
+         title="Train Today"
+         color="#f194ff"
+         onPress={() => Alert.alert('Button with adjusted color pressed')}
+         />
+         <Text style={styles.title}>
+          Focused Muscle Group Workouts
+         </Text>
+       </View>
+       <Separator />
+       <View>
+         <Button style={styles.button}
+           title="Run Today"
+           color="rebeccapurple"
+           onPress={() => Alert.alert('Button with adjusted color pressed')}
+         />
+         <Text style={styles.title}>
+          Track Your Mile Time
+         </Text>
+       </View>
+     </SafeAreaView>
+   );
+ }
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
-
-export default App;
+ const styles = StyleSheet.create({
+   container: {
+     flex: 1,
+     marginHorizontal: 16
+   },
+   title: {
+     textAlign: 'center',
+     marginVertical: 8,
+     fontSize: 18
+   },
+   separator: {
+     marginVertical: 8,
+     borderBottomColor: '#737373',
+     borderBottomWidth: StyleSheet.hairlineWidth,
+   },
+   button: {
+     fontSize: 32,
+     color: '#f194ff'
+   }
+ });
