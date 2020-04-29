ea065608fa48f431697a25b1576ef11bcc5d478a
Virginia Ladd <virginialadd2@gmail.com>
2020-04-28 19:14:48 -0600

    Add a background image to codebase

--- App.js
+++ App.js
@@ -1,48 +1,71 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+*
+* @format
+* @flow strict-local
+*/
 
- import React from 'react';
- import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TouchableOpacity, ImageBackground } from 'react-native';
 
- function Separator() {
-   return <View style={styles.separator} />;
- }
+function Separator() {
+  return <View style={styles.separator} />;
+}
 
- export default function App() {
-   return (
-     <SafeAreaView style={styles.container}>
-       <View>
-         <TouchableOpacity style={{height:50,backgroundColor:"skyblue",alignItems:'center',justifyContent:'center'}}>
-            <Text style={{fontSize:32,}}>Train Today</Text>
-         </TouchableOpacity>
-       </View>
-       <Separator />
-       <View>
-         <TouchableOpacity style={{height:50,backgroundColor:"skyblue",alignItems:'center',justifyContent:'center'}}>
-            <Text style={{fontSize:32,}}>Run Today</Text>
-         </TouchableOpacity>
-       </View>
-     </SafeAreaView>
-   );
- }
+const image = { url: '/Users/virginialadd/projects/wet-tits/wetTits/assets/abs-bg.jpg' };
 
- const styles = StyleSheet.create({
-   container: {
-     flex: 1,
-     marginHorizontal: 16
-   },
-   separator: {
-     marginVertical: 8,
-     borderBottomColor: '#737373',
-     borderBottomWidth: StyleSheet.hairlineWidth,
-   },
-   button: {
-     fontSize: 32,
-     color: '#f194ff'
-   }
- });
+export default function App() {
+  return (
+    <SafeAreaView style={ styles.container }>
+    <ImageBackground source={ image } style={ styles.image } />
+      <View>
+        <TouchableOpacity style={ styles.buttonTop } >
+          <Text style={{ fontSize: 32, color: 'lightgray' }}>Train Today</Text>
+        </TouchableOpacity>
+      </View>
+      <Separator />
+      <View>
+        <TouchableOpacity style={ styles.buttonBottom } >
+          <Text style={{ fontSize: 32, color: 'lightgray' }}>Run Today</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 2,
+    marginHorizontal: 16,
+    color: '#000',
+  },
+  separator: {
+    marginVertical: 8,
+    borderBottomColor: 'lightgray',
+    borderBottomWidth: 2,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'stretch',
+    zIndex: 1,
+  },
+  buttonTop: {
+    height: 100,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    borderTopRightRadius: 50,
+    borderTopLeftRadius: 50,
+    justifyContent:'center',
+    zIndex: 10,
+  },
+  buttonBottom: {
+    height: 100,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    borderBottomRightRadius: 50,
+    borderBottomLeftRadius: 50,
+    justifyContent:'center',
+    zIndex: 10,
+  },
+});
