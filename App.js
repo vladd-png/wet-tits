161e65cb42f4176179451be28019769e5cd5f7a6
Virginia Ladd <virginialadd2@gmail.com>
2020-04-28 18:22:38 -0600

    Got a working and editable button on page

--- App.js
+++ App.js
@@ -7,7 +7,7 @@
  */
 
  import React from 'react';
- import { StyleSheet, Button, View, SafeAreaView, Text, Alert } from 'react-native';
+ import { StyleSheet, Button, View, SafeAreaView, Text, Alert, TouchableOpacity } from 'react-native';
 
  function Separator() {
    return <View style={styles.separator} />;
@@ -17,25 +17,15 @@
    return (
      <SafeAreaView style={styles.container}>
        <View>
-         <Button
-         title="Train Today"
-         color="#f194ff"
-         onPress={() => Alert.alert('Button with adjusted color pressed')}
-         />
-         <Text style={styles.title}>
-          Focused Muscle Group Workouts
-         </Text>
+         <TouchableOpacity style={{height:50,backgroundColor:"skyblue",alignItems:'center',justifyContent:'center'}}>
+            <Text style={{fontSize:32,}}>Train Today</Text>
+         </TouchableOpacity>
        </View>
        <Separator />
        <View>
-         <Button style={styles.button}
-           title="Run Today"
-           color="rebeccapurple"
-           onPress={() => Alert.alert('Button with adjusted color pressed')}
-         />
-         <Text style={styles.title}>
-          Track Your Mile Time
-         </Text>
+         <TouchableOpacity style={{height:50,backgroundColor:"skyblue",alignItems:'center',justifyContent:'center'}}>
+            <Text style={{fontSize:32,}}>Run Today</Text>
+         </TouchableOpacity>
        </View>
      </SafeAreaView>
    );
@@ -45,11 +35,6 @@
    container: {
      flex: 1,
      marginHorizontal: 16
-   },
-   title: {
-     textAlign: 'center',
-     marginVertical: 8,
-     fontSize: 18
    },
    separator: {
      marginVertical: 8,
